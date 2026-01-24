2525b294a6204d775e3c372cee329cff97a61a38
Isaac Lantigua <120843901+Zackfstr18@users.noreply.github.com>
2026-01-24 18:39:11 -0400

    Feat: Agregar Obtener y Enviar Tareas
    Feat: Agregar Obtener y Enviar Tareas:
    
    - Se agrego controller "TaskController"
    - Se agrego Modelo de datos "TaskItem"
    - Se agregaron metodos Get para obtener las tareas, todas y por Id.
    - Se agrego metodo POST para guardar tareas nuevas

---
+++ TaskManagerAPI/Controllers/TaskController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        #region Definitions
+        private static List<TaskItem> tasks = new List<TaskItem>();
+        #endregion
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItem task)
+        {
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+
+            return Ok(task);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id) 
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null) 
+            {
+                return NotFound("Tarea no Encontrada");
+            }
+
+            return Ok(task);
+        }
+    }
+}

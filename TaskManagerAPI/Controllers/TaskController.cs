f6914ca394738358131f36f2f07988a9b99a4260
Isaac Lantigua <120843901+Zackfstr18@users.noreply.github.com>
2026-01-26 09:34:14 -0400

    Feat(Task): Agregar CRUD en TaskController (#2)
    * Feat: Agregar Obtener y Enviar Tareas
    
    Feat: Agregar Obtener y Enviar Tareas:
    
    - Se agrego controller "TaskController"
    - Se agrego Modelo de datos "TaskItem"
    - Se agregaron metodos Get para obtener las tareas, todas y por Id.
    - Se agrego metodo POST para guardar tareas nuevas
    
    * Feat(Tasks): Update y Delete Tasks
    
    Feat(Tasks): Update y Delete Tasks:
    
    - Se agrego metodo Update "CompleteTask()" para marcar las tareas como completadas.
    
    - Se agrego metodo DeleteTasks para remover tareas.

---
+++ TaskManagerAPI/Controllers/TaskController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        #region Definitions
+        private static List<TaskItem> tasks = new List<TaskItem>();
+        #endregion
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskItem task)
+        {
+            task.Id = tasks.Count + 1;
+            tasks.Add(task);
+
+            return Ok(task);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id) 
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null) 
+            {
+                return NotFound("Tarea no Encontrada");
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPut("{id}/complete")]
+        public IActionResult CompleteTask(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound("Tarea no encontrada");
+            }
+
+            task.IsCompleted = true;
+
+            return Ok(task);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound("Tarea no encontrada");
+            }
+
+            tasks.Remove(task);
+
+            return NoContent();
+        }
+    }
+}
